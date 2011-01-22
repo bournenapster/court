bb84712f3476e70a1e3810d0bb90438a5d037ef4
= <=>
2011-01-22 20:53:47 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Court::Application.config.secret_token = 'd5f688d8068ada3cce0a7ea368875a0d1064160221a7c4e86890b637c6d778b0c6cd6f30c55e0721527ceee8673733055ea2c9c97213578f15d169d5996d0b68'
